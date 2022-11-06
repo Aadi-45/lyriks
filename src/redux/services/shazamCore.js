0e18b40ccd1e18f012799bcb2bb454e2aa003aff
Aditya <aditya080102@gmail.com>
2022-11-06 12:20:41 +0530

    Implemented Search, Top Artist, Top Charts

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,26 +1,47 @@
-
-import {createApi,fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const shazamCoreApi = createApi({
-    reducerPath:'shazamCoreApi',
-    baseQuery:fetchBaseQuery({
-        baseUrl:'https://shazam-core.p.rapidapi.com/v1',
-        prepareHeaders:(headers)=>{
-            headers.set( 'X-RapidAPI-Key','24f42a2232mshb2c47419f543e07p13d4cbjsn55b2418cfefc');
+  reducerPath: "shazamCoreApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://shazam-core.p.rapidapi.com/v1",
+    prepareHeaders: (headers) => {
+      headers.set(
+        "X-RapidAPI-Key",
+        "24f42a2232mshb2c47419f543e07p13d4cbjsn55b2418cfefc"
+      );
 
-            return headers;
-        },
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => "/charts/world" }),
+    getSongsByGenre: builder.query({
+      query: (genre) => `/charts/genre-world?genre_code=${genre}`,
     }),
-    endpoints:(builder)=>({
-        getTopCharts:builder.query({query:()=>'/charts/world'}),
-        getSongDetails:builder.query({query:({songid})=>`/tracks/details?track_id=${songid}` }),
-        getSongRelated: builder.query({ query: ({ songid }) => `/tracks/related?track_id=${songid}` }),
+    getSongDetails: builder.query({
+      query: ({ songid }) => `/tracks/details?track_id=${songid}`,
     }),
+    getSongRelated: builder.query({
+      query: ({ songid }) => `/tracks/related?track_id=${songid}`,
+    }),
+    getArtistDetails: builder.query({
+      query: (artistId) => `/artists/details?artist_id=${artistId}`,
+    }),
+    getSongsByCountry: builder.query({
+      query: (countryCode) => `/charts/country?country_code=${countryCode}`,
+    }),
+    getSongsBySearch: builder.query({ query: (searchTerm) => `/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}` }),
 
+
+  }),
 });
 
 export const {
-    useGetTopChartsQuery,
-    useGetSongDetailsQuery,
-    useGetSongRelatedQuery,
-}=shazamCoreApi;
+  useGetTopChartsQuery,
+  useGetSongsByGenreQuery,
+  useGetSongDetailsQuery,
+  useGetSongRelatedQuery,
+  useGetArtistDetailsQuery,
+  useGetSongsByCountryQuery,
+  useGetSongsBySearchQuery,
+} = shazamCoreApi;
